dd6da2f1c387e10907610a5d7eb640e2fef4de0d
qf X <2694561878@qq.com>
2023-12-03 16:23:01 +0800

    front-end test

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { useUserStore } from '@/stores/userStore'
+import router from './router'
+const userStore = useUserStore()
+const username = userStore.userInfo.username
+
+
+const logout = () => {
+
+  router.push({name:'login'})
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+
+.el-menu-demo{
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px #888888;
+}
+
+.hello{
+  position: absolute;
+  margin-top: 18px;
+  margin-left: 87%;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login/index.vue'
+import Introduction from '@/views/Introduction/index.vue'
+import ScholarView from '../views/scholar/index.vue'
+import AcademicView from '../views/academic/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // path和component对应关系的位置
+  routes: [
+    {
+      path: '/',
+      name: 'introduction',
+      component: Introduction
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/scholar',
+      name: 'scholar',
+      component: ScholarView
+    },
+    {
+      path: '/academic',
+      name: 'academic',
+      component: AcademicView
+    }
+  ]
+})
+
+export default router
